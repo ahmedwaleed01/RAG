4417558f3d740ed37f14aa5b379dec4e8942a618
ahmedwaleed01 <ahmed.waleed1303@gmail.com>
2025-09-13 16:59:51 +0300

    Docker cleanup commands and indexing methods

--- README.md
+++ README.md
@@ -15,3 +15,33 @@ uvicorn main:app --reload
 ```bash
 docker compose -f docker-compose.yml up -d --build
 ```
+
+### Stop Docker
+
+```bash
+sudo docker stop $(sudo docker ps -qa)
+```
+
+### Remove Docker
+
+```bash
+sudo docker rm $(sudo docker ps -qa)
+```
+
+### Remove image
+
+```bash
+sudo docker rmi $(sudo docker images -qa)
+```
+
+## Remove volumes
+
+```bash
+sudo docker volume rm $(sudo docker volume ls -q)
+```
+
+### Remove all
+
+```bash
+ sudo docker system prune --all
+```
--- src/routes/data.py
+++ src/routes/data.py
@@ -17,7 +17,7 @@ data_router = APIRouter(
 async def upload_data(request:Request,project_id: str, file: UploadFile,
                        app_settings: Settings = Depends(get_settings)):
     
-    project_model = ProjectModel.create_instance(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
     project = await project_model.get_project_or_create_one(project_id=project_id)
     # validate file type
     data_controller = DataController()
